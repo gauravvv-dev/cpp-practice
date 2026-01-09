c8511b13ade2ce33dbdfa165192e267c86517bde
Gaurav <gauravcr7.vvs@gmail.com>
2026-01-09 23:01:23 +0530

    just few more learning in operators

---
+++ First Program/04_constant.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // The const keyword specifies that a variable's value is constant
+    // tells the compiler to prevent anything from modifying it 
+    // (read only)
+
+
+    const double PI = 3.14159;              //when using const use the variable name in uppercase.
+    
+    const int LIGHT_SPEED = 2999792458;
+    const int WIDTH = 1920;
+    const int HEIGHT = 1080;
+}
--- First Program/2_datatype_&_variables.cpp
+++ First Program/2_datatype_&_variables.cpp
@@ -2,24 +2,37 @@
 using namespace std;
 int main()
 {
-    /*int a = 123;
-    cout << a << endl;
+    // int that stores whole numbers 
+    int a = 123;
+    //cout << a << endl;
+
+    // char that stores a single character 
     char b = 'i';
-    cout << b << endl;
-    bool bl = true;
-    cout << bl << endl;
-    float f = 1.2;
-    cout << f << endl;
-    double d = 1.23;
-    cout << d << endl;
-    int size = sizeof(a);
-    cout<< "Size of a is:" << size << endl;
-
-    int a = 'a';
-    cout<<a<<endl;
-    char ch = 98;
-    cout << ch << endl;*/
-
     
+    //boolean (true or false)
+    bool student = true;
+    
+
+    // float that stores decimal value 
+    float f = 1.2;
+    
+    // double that also stores decimal
+    double d = 1.23;
+    
+    //size of that tells the size of the data type 
+    int size = sizeof(a);
+   
+    // type conversions 
+    int a = 'a';
+    char ch = 98;
+
+    // string (object that represent a sequence of text)
+
+    string name = "Bro";
+    string day = "Friday";
+    string food = "Pizza";
+    string address = "123 Fake St.";
+    cout << "Hello " << name << '\n';
+    cout << "Today is "<< day << endl;
 
 }

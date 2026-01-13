a59543c6bf83d69830cdf0c190635322f1c92b4f
Gaurav <gauravcr7.vvs@gmail.com>
2026-01-13 21:01:48 +0530

    Day 2

--- Programs/09_user_input.cpp
+++ Programs/09_user_input.cpp
@@ -3,11 +3,18 @@ using namespace std;
 
 int main() {
 
+    // cout << (insertion operator)
+    // cin >> (extraction operator)
+
     string name;
     int age;
+    int clas ;
 
     cout<< "What's your age :";
     cin>> age ;
+
+    cout << "In which class you study ?";
+    cin >> clas ;
 
     cout << "What's your name ?:";
     getline(cin >> ws, name);               //getline function helps us to enter the name with spaces. Without getline when using a string we cant use spaces 
